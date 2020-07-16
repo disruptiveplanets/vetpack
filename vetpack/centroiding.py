8268f9f7118a51114ccc50ca83a7c4a53b55d534
Benjamin Rackham <brackham@mit.edu>
2020-07-16 14:41:01 -0400

    Make plot_pipeline_mask() an internal function

--- vetpack/centroiding.py
+++ vetpack/centroiding.py
@@ -6,19 +6,6 @@ from matplotlib import patches
 from scipy.signal import find_peaks
 
 
-def plot_pipeline_mask(ax, tpf):
-    for i, j in np.ndindex(tpf.pipeline_mask.shape):
-        if tpf.pipeline_mask[i, j]:
-            ax.add_patch(
-                patches.Rectangle(
-                    (j+tpf.column, i+tpf.row), 1, 1,
-                    color='pink', fill=True, alpha=0.5
-                )
-            )
-
-    return ax
-
-
 def median_image(tpf, ax=None):
     # Target position in the TPF
     radec = np.vstack([tpf.ra, tpf.dec]).T
@@ -43,7 +30,7 @@ def median_image(tpf, ax=None):
         med_image, extent=extent, interpolation='nearest', origin='lower'
     )
     ax.plot(tx, ty, 'ro', alpha=0.5)
-    plot_pipeline_mask(ax, tpf)
+    ax = _plot_pipeline_mask(ax, tpf)
     ax.set_xlabel('Pixel Column')
     ax.set_ylabel('Pixel Row')
     ax.set_xlim(xlim)
@@ -103,11 +90,24 @@ def difference_image(tpf, t0, period, duration, ax=None):
         med_diff, extent=extent, interpolation='nearest', origin='lower'
     )
     ax.plot(tx, ty, 'ro', alpha=0.5)
-    plot_pipeline_mask(ax, tpf)
+    ax = _plot_pipeline_mask(ax, tpf)
     ax.set_xlabel('Pixel Column')
     ax.set_ylabel('Pixel Row')
     ax.set_xlim(xlim)
     ax.set_ylim(ylim)
     plt.colorbar(im, label='Flux (e$^{-1} s^{-1}$)')
+
+    return ax
+
+
+def _plot_pipeline_mask(ax, tpf):
+    for i, j in np.ndindex(tpf.pipeline_mask.shape):
+        if tpf.pipeline_mask[i, j]:
+            ax.add_patch(
+                patches.Rectangle(
+                    (j+tpf.column, i+tpf.row), 1, 1,
+                    color='pink', fill=True, alpha=0.5
+                )
+            )
 
     return ax
